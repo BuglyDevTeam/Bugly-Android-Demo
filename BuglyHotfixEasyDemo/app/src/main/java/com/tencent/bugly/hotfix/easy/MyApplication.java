f75338d79dfd0ca8d361c107ad6b4a7e127c3d0f
wenjiewu <wenjiewu@tencent.com>
2017-03-16 18:02:03 +0800

    调整热更新Demo

--- BuglyHotfixEasyDemo/app/src/main/java/com/tencent/bugly/hotfix/easy/MyApplication.java
+++ BuglyHotfixEasyDemo/app/src/main/java/com/tencent/bugly/hotfix/easy/MyApplication.java
@@ -2,14 +2,14 @@ package com.tencent.bugly.hotfix.easy;
 
 import android.app.Application;
 import android.content.Context;
-import android.content.res.Configuration;
 import android.support.multidex.MultiDex;
-import android.util.Log;
+import android.widget.Toast;
 
 import com.tencent.bugly.Bugly;
 import com.tencent.bugly.beta.Beta;
-import com.tencent.bugly.beta.tinker.TinkerApplicationLike;
-import com.tencent.tinker.loader.app.ApplicationLike;
+import com.tencent.bugly.beta.interfaces.BetaPatchListener;
+
+import java.util.Locale;
 
 /**
  * Demo Application示例.
@@ -23,8 +23,54 @@ public class MyApplication extends Application {
     public void onCreate() {
         super.onCreate();
 
-        // 这里实现SDK初始化，appId替换成你的在Bugly平台申请的appId
-        Bugly.init(this, "900029763", false);
+        // 设置是否自动下载补丁
+        Beta.canAutoDownloadPatch = true;
+        // 设置是否提示用户重启
+        Beta.canNotifyUserRestart = false;
+        // 设置是否自动合成补丁
+        Beta.canAutoPatch = true;
+
+        /**
+         * 补丁回调接口，可以监听补丁接收、下载、合成的回调
+         */
+        Beta.betaPatchListener = new BetaPatchListener() {
+            @Override
+            public void onPatchReceived(String patchFileUrl) {
+                Toast.makeText(getApplicationContext(), patchFileUrl, Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onDownloadReceived(long savedLength, long totalLength) {
+                Toast.makeText(getApplicationContext(), String.format(Locale.getDefault(),
+                        "%s %d%%",
+                        Beta.strNotificationDownloading,
+                        (int) (totalLength == 0 ? 0 : savedLength * 100 / totalLength)), Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onDownloadSuccess(String patchFilePath) {
+                Toast.makeText(getApplicationContext(), patchFilePath, Toast.LENGTH_SHORT).show();
+                Beta.applyDownloadedPatch();
+            }
+
+            @Override
+            public void onDownloadFailure(String msg) {
+                Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onApplySuccess(String msg) {
+                Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onApplyFailure(String msg) {
+                Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_SHORT).show();
+            }
+        };
+
+        // 这里实现SDK初始化，appId替换成你的在Bugly平台申请的appId,调试时将第三个参数设置为true
+        Bugly.init(this, "900029763", true);
 }
 
     @Override
