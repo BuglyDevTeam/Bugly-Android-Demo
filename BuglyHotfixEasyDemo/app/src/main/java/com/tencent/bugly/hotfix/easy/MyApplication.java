868e07a199a5d33d3680f461d4387beae2820310
wenjiewu <wenjiewu@tencent.com>
2017-04-13 12:04:27 +0800

    更新一键接入热更新示例工程

--- BuglyHotfixEasyDemo/app/src/main/java/com/tencent/bugly/hotfix/easy/MyApplication.java
+++ BuglyHotfixEasyDemo/app/src/main/java/com/tencent/bugly/hotfix/easy/MyApplication.java
@@ -1,13 +1,17 @@
 package com.tencent.bugly.hotfix.easy;
 
+import android.annotation.TargetApi;
 import android.app.Application;
 import android.content.Context;
+import android.os.StrictMode;
 import android.support.multidex.MultiDex;
+import android.util.Log;
 import android.widget.Toast;
 
 import com.tencent.bugly.Bugly;
 import com.tencent.bugly.beta.Beta;
 import com.tencent.bugly.beta.interfaces.BetaPatchListener;
+import com.tencent.bugly.beta.upgrade.UpgradeStateListener;
 
 import java.util.Locale;
 
@@ -23,12 +27,45 @@ public class MyApplication extends Application {
     public void onCreate() {
         super.onCreate();
 
+        setStrictMode();
+        // 设置是否关闭热更新能力，默认为true
+        Beta.enableHotfix = true;
         // 设置是否自动下载补丁
-        Beta.canAutoDownloadPatch = true;
+        Beta.canAutoDownloadPatch = false;
         // 设置是否提示用户重启
         Beta.canNotifyUserRestart = false;
         // 设置是否自动合成补丁
-        Beta.canAutoPatch = true;
+        Beta.canAutoPatch = false;
+
+        /**
+         *  全量升级状态回调
+         */
+        /*Beta.upgradeStateListener = new UpgradeStateListener() {
+            @Override
+            public void onUpgradeFailed(boolean b) {
+
+            }
+
+            @Override
+            public void onUpgradeSuccess(boolean b) {
+
+            }
+
+            @Override
+            public void onUpgradeNoVersion(boolean b) {
+                Toast.makeText(getApplicationContext(), "最新版本", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onUpgrading(boolean b) {
+                Toast.makeText(getApplicationContext(), "onUpgrading", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onDownloadCompleted(boolean b) {
+
+            }
+        };*/
 
         /**
          * 补丁回调接口，可以监听补丁接收、下载、合成的回调
@@ -69,8 +106,11 @@ public class MyApplication extends Application {
             }
         };
 
+        long start = System.currentTimeMillis();
         // 这里实现SDK初始化，appId替换成你的在Bugly平台申请的appId,调试时将第三个参数设置为true
-        Bugly.init(this, "900029763", true);
+        Bugly.init(this, "900029763", false);
+        long end = System.currentTimeMillis();
+        Log.e("init time--->", end - start + "ms");
 }
 
     @Override
@@ -83,4 +123,10 @@ public class MyApplication extends Application {
         Beta.installTinker();
     }
 
+
+    @TargetApi(9)
+    protected void setStrictMode() {
+        StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.Builder().permitAll().build());
+        StrictMode.setVmPolicy(new StrictMode.VmPolicy.Builder().detectAll().penaltyLog().build());
+    }
 }
