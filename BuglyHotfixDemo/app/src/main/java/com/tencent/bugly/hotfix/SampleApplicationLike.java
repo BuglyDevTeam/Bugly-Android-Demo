2a56dcd3d9218baaf052087bc7845c12f2bf1a26
rocky <zhhp1121@sina.com>
2019-03-29 17:35:38 +0800

    优化demo，1.增加tinker-support注释2.增加demo test apk给用户做参照

--- BuglyHotfixDemo/app/src/main/java/com/tencent/bugly/hotfix/SampleApplicationLike.java
+++ BuglyHotfixDemo/app/src/main/java/com/tencent/bugly/hotfix/SampleApplicationLike.java
@@ -97,7 +97,7 @@ public class SampleApplicationLike extends DefaultApplicationLike {
         // String channel = WalleChannelReader.getChannel(getApplication());
         // Bugly.setAppChannel(getApplication(), channel);
         // 这里实现SDK初始化，appId替换成你的在Bugly平台申请的appId
-        Bugly.init(getApplication(), "900029763", true);
+        Bugly.init(getApplication(), "e0b1ba785f", true);
     }
 
 
