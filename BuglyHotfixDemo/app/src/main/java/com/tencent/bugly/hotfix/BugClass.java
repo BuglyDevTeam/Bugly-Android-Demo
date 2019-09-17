eb45fd9e6df260120729f58b8cd4e7236e6c98d1
rockypzhang <rockypzhang@tencent.com>
2019-09-17 15:28:58 +0800

    更新tinker 到 1.9.14

--- BuglyHotfixDemo/app/src/main/java/com/tencent/bugly/hotfix/BugClass.java
+++ BuglyHotfixDemo/app/src/main/java/com/tencent/bugly/hotfix/BugClass.java
@@ -12,8 +12,8 @@ public class BugClass {
 
     public String bug() {
         // 这段代码会报空指针异常
-        String str = null;
-        Log.e("BugClass", "get String length:" + str.length());
-        return "This is a bug class";
+        //String str = null;
+        //Log.e("BugClass", "get String length:" + str.length());
+        return "This is a bug class 2";
     }
 }
