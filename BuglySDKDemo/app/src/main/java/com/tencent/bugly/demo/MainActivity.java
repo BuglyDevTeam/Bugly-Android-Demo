a57234a2b6acd7ebe9f7bb5ff710fac5c5d294e4
rocky <zhhp1121@sina.com>
2020-06-12 19:31:30 +0800

    crashdemo/upgradedemo适配到androidx

--- BuglySDKDemo/app/src/main/java/com/tencent/bugly/demo/MainActivity.java
+++ BuglySDKDemo/app/src/main/java/com/tencent/bugly/demo/MainActivity.java
@@ -4,7 +4,7 @@ import com.tencent.bugly.crashreport.CrashReport;
 import com.tencent.bugly.demo.mockcrash.CrashMock;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
--- BuglyUpgradeDemo/app/src/main/java/com/bugly/upgrade/demo/MainActivity.java
+++ BuglyUpgradeDemo/app/src/main/java/com/bugly/upgrade/demo/MainActivity.java
@@ -1,8 +1,8 @@
 package com.bugly.upgrade.demo;
 
 import android.os.Bundle;
-import android.support.annotation.Nullable;
-import android.support.v7.app.AppCompatActivity;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
